390d51830dbb88fd6a5c4bbc684900fe51264694
MihailNn <mihailnaryshkin@gmail.com>
2018-02-26 13:20:19 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let b = preferences[i];
   let c = preferences[b-1];
   let d = preferences[c-1];
-  console.log(a, b, c, d);
   if (a !== c && a === d) {
     count += 1;
-    console.log(count);
     preferences.length--;
     }
   else {
